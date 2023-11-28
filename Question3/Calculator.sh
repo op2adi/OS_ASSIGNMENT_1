6ca930a158083bab17d5b0cc26c9ef119ba0b4af
op2adi <adityaupadhyay2708@gmail.com>
2023-11-28 23:09:18 +0530

    Initial commit

---
+++ Question3/Calculator.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+out_directory="/home/sahilg/Desktop/OS-assignments-/operating_system/Assignment-1/Question3/Result"
+
+out_file="$out_directory/output.txt"
+
+mkdir -p "$out_directory"
+
+> "$out_file"
+#echo "$out_file"
+while IFS= read -r line || [ -n "$line" ]; do
+    if [ -n "$line" ]; then
+        x=$(echo "$line" | awk '{print $1}')
+        y=$(echo "$line" | awk '{print $2}')
+        operation=$(echo "$line" | awk '{print $3}')
+        #echo "$operation"
+        if [ "$operation" = "xor" ]; then       
+            result=$((y^x))
+            echo "Result of xor $x $y : $result" >> "$out_file"
+        elif [ "$operation" = "product" ]; then
+            result=$((x * y))
+            echo "Result of product $x $y : $result" >> "$out_file"
+        elif [ "$operation" = "compare" ];then
+            if [ "$x" -gt "$y" ]; then
+                echo "Result of compare $x $y : $x" >> "$out_file"
+            else 
+                echo "Result of compare $x $y : $y" >> "$out_file"
+            fi
+        else 
+            echo "No such operation" >> "$out_file"    
+        fi
+    fi
+done < "/home/sahilg/Desktop/OS-assignments-/operating_system/Assignment-1/Question3/input.txt"
